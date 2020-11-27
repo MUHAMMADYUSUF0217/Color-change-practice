9feaebd47aa6c63fa45dcb665a98d32582bedb31
MUHAMMADYUSUF0217 <muhammadyusufurmonov0217@gmail.com>
2020-11-27 11:25:02 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+ let body = document.querySelector('body')	
+ 
+let colors = {
+	setColors: function (color){
+		body.style.backgroundColor = color
+	}
+}
+	btn.onclick = function(){
+		colors.setColors(select.value)
+	}
+
+
+
+ 
